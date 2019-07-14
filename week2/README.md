97d091ef03c59e7a629d91671907ee9b49f90631
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-07-14 21:33:49 +0545

    Added new python code to convert dicom to jpeg using opencv

---
+++ week2/README.md
@@ -0,0 +1,5 @@
+## [x] Week 2
+* Convert DCM Format to jpeg or other image types:
+https://www.kaggle.com/onealbao/dicom-to-jpeg-conversion-kernel
+* Getting Started: A reference model framework for getting started on this dataset can be reviewed. https://www.kaggle.com/juliaelliott/siim-reference-model
+
---
+++ week2/local/dicom-to-jpeg.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import pydicom
+import glob
+
+# As per my setup, input data files are in "dicom-images-test" directory at the root folder. The
+# output folder is 'output_dir'. You may change the path as per your file structure.
+
+######### Dependencies ################
+#
+
+inputdir = 'dicom-images-test'
+outdir = 'output-dir'
+
+test_list = [os.path.basename(x) for x in glob.glob(inputdir + './*.dcm')]
+# get all dcm files from the test folder
+def getAllFiles(path):
+    dcmFiles = []
+    for dirpath, subdirs, files in os.walk(path):
+        for x in files:
+            if x.endswith(".dcm"):
+                dcmFiles.append([dirpath, x])
+    return dcmFiles
+
+allDCMImages = getAllFiles(inputdir)
+
+for file in allDCMImages:
+    # file[0] contains path
+    # file[1] contains file name
+    filename = os.path.join(file[0],file[1])
+    ds = pydicom.read_file(filename)  # read dicom image
+    img = ds.pixel_array  # get image array
+
+    #print(filename)
+    print(outdir + "/" + file[0])
+    if not os.path.exists(outdir + "/" + file[0]):
+        os.makedirs(outdir + "/" + file[0])
+
+    cv2.imwrite(outdir + "/" + filename.replace('.dcm', '.png'), img)  # write png image
